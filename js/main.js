a58f7c7b98440b69d1470912caca3c5dccbab4b3
Edoardo <ercolino.edoardo@gmail.com>
2021-07-19 16:15:50 +0200

    Conclusione esercitazione

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 // Scrivi un programma che stampi i numeri da 1 a 100
-var numeri = 100;
+var numerazione = 100;
 
-for (i = 0; i < numeri; i++) {
-    console.log(i + 1);
+for (var i = 0; i < numerazione; i++) {
+    var numero = i + 1;
+
+    // -Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    if (numero % 3 == 0 && numero % 5 == 0) {
+        console.log('FizzBuzz')
+    }
+    // -Per i multipli di 3 stampi “Fizz”  
+    else if (numero % 3 == 0) {
+        console.log('Fizz')
+    }
+    // -Per i multipli di 5 stampi Buzz.
+    else if (numero % 5 == 0) {
+        console.log('Buzz')
+    }
+    else {
+        console.log(numero);
+    }
 }
-// ma:
-// -per i multipli di 3 stampi “Fizz”  
-// -per i multipli di 5 stampi Buzz.
 
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
